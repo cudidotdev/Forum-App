28ff136d02b7b3d34edb52a0558cc64360567018
Cudilala <augustinemadu9@gmail.com>
2023-05-21 01:31:06 +0100

    title and favicon update

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,6 @@
+<svelte:head>
+	<title>A Forum application with svelkit and rust by CudiLala</title>
+	<meta name="description" content="A forum application with sveltekit and rust by CudiLala" />
+</svelte:head>
+
+<slot />
