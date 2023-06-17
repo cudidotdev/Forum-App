219e85ce22ab725675160fc77bf69e078c8c1176
Cudilala <augustinemadu9@gmail.com>
2023-06-18 00:33:47 +0100

    update server url

--- src/lib/api/index.ts
+++ src/lib/api/index.ts
@@ -3,7 +3,7 @@ import { browser, dev } from '$app/environment';
 import posts from './posts';
 
 const api = {
-	url: () => (browser || dev ? 'http://127.0.0.1:8080' : 'https://api.forum.projects.cudilala.io'),
+	url: () => (browser || dev ? 'http://127.0.0.1:8080' : 'http://3.212.5.169:8080'),
 	auth,
 	posts
 };
