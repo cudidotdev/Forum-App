6d6b88157dc75ba65ab9ab5fa2f1db3c8fed26b4
Ubuntu <ubuntu@ip-172-31-94-34.ec2.internal>
2023-06-18 08:22:28 +0000

    url error fix from server

--- src/lib/api/index.ts
+++ src/lib/api/index.ts
@@ -1,9 +1,9 @@
 import auth from './auth';
-import { browser, dev } from '$app/environment';
+import { dev } from '$app/environment';
 import posts from './posts';
 
 const api = {
-	url: () => (!browser || dev ? 'http://127.0.0.1:8080' : 'http://3.212.5.169:8080'),
+	url: () => (dev ? 'http://127.0.0.1:8080' : 'http://3.212.5.169:8080'),
 	auth,
 	posts
 };
