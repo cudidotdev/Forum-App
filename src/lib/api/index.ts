2fd599e0f8790420ec986f3c4ad3b78cd270639d
Cudilala <augustinemadu9@gmail.com>
2023-06-22 01:49:32 +0100

    update api url for digital ocean server

--- src/lib/api/index.ts
+++ src/lib/api/index.ts
@@ -3,7 +3,7 @@ import { browser, building, dev } from '$app/environment';
 import posts from './posts';
 
 const api = {
-	url: () => (!browser || building || dev ? 'http://127.0.0.1:8080' : 'http://3.212.5.169:8080'),
+	url: () => (!browser || building || dev ? 'http://127.0.0.1:8080' : 'http://157.245.29.181:8080'),
 	auth,
 	posts
 };
