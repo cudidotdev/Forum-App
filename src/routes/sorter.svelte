87bd86d6f0342a76e1bce674efeb7fb80f714ff1
Cudilala <augustinemadu9@gmail.com>
2023-06-22 16:41:41 +0100

    add post sorting in homepage

--- src/routes/sorter.svelte
+++ src/routes/sorter.svelte
@@ -1,24 +1,33 @@
 <script>
 	import BalloonIcon from '$lib/icons/balloon-icon.svelte';
 	import RocketIcon from '$lib/icons/rocket-icon.svelte';
+	import { page } from '$app/stores';
+
+	$: sort = $page.url.searchParams.get('sort') || 'highest';
 </script>
 
 <div class="flex flex-col gap-2">
 	<div class="box px-4 py-2 flex justify-start items-center gap-4">
-		<button class="flex-grow-0 flex-shrink-0">
-			<div class="box !bg-neutral-50 !w-fit font-bold text-lg py-1 px-2 flex gap-2">
-				<span class="flex w-6 h-6">
-					<BalloonIcon />
-				</span>
-				New
-			</div>
-		</button>
+		<a
+			href="/?sort=latest"
+			class="flex-grow-0 flex-shrink-0 font-bold text-lg py-1 px-2 flex gap-2 w-fit
+      {sort === 'latest' ? 'box !bg-neutral-50' : ''}"
+		>
+			<span class="flex w-6 h-6">
+				<BalloonIcon />
+			</span>
+			New
+		</a>
 
-		<button class="w-fit font-bold text-lg py-1 px-2 flex gap-2 flex-grow-0 flex-shrink-0">
+		<a
+			href="/?sort=highest"
+			class="w-fit font-bold text-lg py-1 px-2 flex gap-2 flex-grow-0 flex-shrink-0
+      {sort === 'highest' ? 'box !bg-neutral-50' : ''}"
+		>
 			<span class="flex w-6 h-6">
 				<RocketIcon />
 			</span>
 			Top
-		</button>
+		</a>
 	</div>
 </div>
