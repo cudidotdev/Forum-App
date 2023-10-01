6d69bd9a63899dfc233233d23688af9110126b12
CudiLala <augustinemadu9@gmail.com>
2023-10-01 13:39:30 +0100

    modified seo tags

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -33,8 +33,35 @@
 </script>
 
 <svelte:head>
-	<title>A Forum application with svelkit and rust by CudiLala</title>
-	<meta name="description" content="A forum application with sveltekit and rust by CudiLala" />
+	<title>A Forum application with svelkit and rust by Augustine Madu</title>
+	<meta
+		name="description"
+		content="A forum application with sveltekit and rust by Augustine Madu"
+	/>
+	<meta
+		name="keywords"
+		content="Forum application, Svelte, Rust, Rust side projects, Svelte, side projects, side projects, Actix web side project, Actix web, Actix Web sample project, Web Application Developer, Job, Forum, Reddit projects, Javascript, Typescript, Rust"
+	/>
+	<meta property="og:title" content="A Forum application with svelkit and rust by CudiLala" />
+	<meta
+		property="og:description"
+		content="A forum application with sveltekit and rust by Augustine Madu"
+	/>
+	<meta property="og:url" content="https://forum.projects.cudi.dev" />
+	<meta property="og:type" content="website" />
+	<meta property="og:locale" content="en_GB" />
+	<meta property="og:image" content="https://forum.projects.cudi.dev/ogimage.png" />
+	<meta property="og:image:width" content="1551" />
+	<meta property="og:image:height" content="917" />
+
+	<meta
+		name="twitter:description"
+		content="A forum application with sveltekit and rust by Augustine Madu"
+	/>
+	<meta name="twitter:card" content="summary" />
+	<meta name="twitter:site" content="@CudiLala_" />
+	<meta property="twitter:url" content="https://forum.projects.cudi.dev" />
+	<meta property="twitter:image" content="https://forum.projects.cudi.dev/ogimage.png" />
 </svelte:head>
 
 <Notification />
